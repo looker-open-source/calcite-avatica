looker-open-source/calcite-avatica

commit d1e51684c5908b4701d4efcc5c704c2592ad1447

[b/268509716] [b/250969501] Add array support for primitive types.  (#5)
* Add array support for primitive types
---------

Co-authored-by: Justin Swett <jswett@google.com>

core/src/main/java/org/apache/calcite/avatica/remote/looker/LookerResponseParser.java
@@ -23,6 +23,8 @@ import org.apache.calcite.avatica.Meta.Signature;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.io.NumberInput;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -71,7 +73,7 @@ public class LookerResponseParser {
     switch (columnMetaData.type.rep) {
     case PRIMITIVE_BOOLEAN:
     case BOOLEAN:
-      return parser.getBooleanValue();
+      return parser.getValueAsBoolean();
     case PRIMITIVE_BYTE:
     case BYTE:
       return parser.getByteValue();
@@ -82,19 +84,23 @@ public class LookerResponseParser {
       return parser.getShortValue();
     case PRIMITIVE_INT:
     case INTEGER:
-      return parser.getIntValue();
+      return parser.getValueAsInt();
     case PRIMITIVE_LONG:
     case LONG:
-      return parser.getLongValue();
+      return parser.getValueAsLong();
     case PRIMITIVE_FLOAT:
     case FLOAT:
       return parser.getFloatValue();
     case PRIMITIVE_DOUBLE:
     case DOUBLE:
-      return parser.getDoubleValue();
+      return parser.getValueAsDouble();
     case NUMBER:
       // NUMBER is represented as BigDecimal
-      return parser.getDecimalValue();
+      if (parser.currentToken() == JsonToken.VALUE_STRING) {
+        return NumberInput.parseBigDecimal(parser.getValueAsString());
+      } else {
+        return parser.getDecimalValue();
+      }
     // TODO: MEASURE types are appearing as Objects. This should have been solved by CALCITE-5549.
     //  Be sure that the signature of a prepared query matches the metadata we see from JDBC.
     case OBJECT:
@@ -112,6 +118,47 @@ public class LookerResponseParser {
     default:
       throw new IOException("Unable to parse " + columnMetaData.type.rep + " from stream!");
     }
+  }
+
+  /**
+   * Collects the values of an array whose elements are represented by {@code metaData.type.rep}.
+   * Does not support nested arrays.
+   *
+   * @param metaData the {@link ColumnMetaData} for this value.
+   * @param parser a JsonParser whose current token is JsonToken.START_ARRAY. The parser is advanced
+   *        through the elements until the end of the array is reached. Parse does not advance past
+   *        END_ARRAY.
+   * @return An array of values with element type matching {@code metaData.type.rep}.
+   */
+  static Object[] deserializeArray(JsonParser parser, ColumnMetaData metaData) throws IOException {
+    assert parser.currentToken() == JsonToken.START_ARRAY
+        : "Invalid parsing state. Expecting start of array.";
+
+    boolean isPrimitive = isPrimitive(metaData);
+    ArrayList result = new ArrayList();
+    while (parser.nextToken() != JsonToken.END_ARRAY) {
+      Object deserialized = deserializeValue(parser, metaData);
+      if (isPrimitive && (deserialized == null)) {
+        throw new IOException("Primitive array cannot contain null values");
+      }
+      result.add(deserialized);
+    }
+    return result.toArray();
+  }
+
+  static boolean isPrimitive(ColumnMetaData metaData) {
+    switch (metaData.type.rep) {
+    case PRIMITIVE_BOOLEAN:
+    case PRIMITIVE_BYTE:
+    case PRIMITIVE_CHAR:
+    case PRIMITIVE_SHORT:
+    case PRIMITIVE_INT:
+    case PRIMITIVE_LONG:
+    case PRIMITIVE_FLOAT:
+    case PRIMITIVE_DOUBLE:
+      return true;
+    }
+    return false;
   }
 
   private void seekToRows(JsonParser parser) throws IOException {
@@ -176,8 +223,13 @@ public class LookerResponseParser {
               return;
             }
 
-            // add the value to the column list
-            columnValues.add(deserializeValue(parser, signature.columns.get(i)));
+            ColumnMetaData metaData = signature.columns.get(i);
+            if (parser.currentToken() == JsonToken.START_ARRAY) {
+              columnValues.add(deserializeArray(parser, metaData));
+            } else {
+              // add the value to the column list
+              columnValues.add(deserializeValue(parser, metaData));
+            }
           }
 
           // Meta.CursorFactory#deduce will select an OBJECT cursor if there is only a single

core/src/test/java/org/apache/calcite/avatica/remote/looker/LookerDriverTest.java
@@ -21,6 +21,7 @@ import org.apache.calcite.avatica.AvaticaConnection;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
@@ -33,6 +34,7 @@ import java.sql.Types;
 import java.util.Properties;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -46,6 +48,21 @@ public class LookerDriverTest {
       "SELECT " + "`users.created_date`, `users.created_year`, `users.created_time`, `users.name`\n"
           + "`users.age`, `users.is45or30`, SUM(`users.age`), AGGREGATE(`users.average_age`)\n"
           + "FROM example.users\n" + "GROUP BY 1, 2, 3, 4, 5, 6";
+
+  static final String TEST_LIST_MEASURE_SQL = "SELECT `users.created_year`\n"
+      + "        , AGGREGATE(`users.names`) as `names`\n"
+      + "        , AGGREGATE(`users.ages`) as `ages`\n"
+      + "       FROM `example`.`users`\n"
+      + "       GROUP BY `users.created_year`\n"
+      + "       LIMIT 2";
+
+  static final String TEST_ARRAY_PRIMITIVES_SQL = "SELECT \"testing arrays\" as `test_name`\n"
+      + "        , array[1,2,3] as `int_arrs`\n"
+      + "        , array[1.1, 2.2, 3.3] as `float_arr`\n"
+      + "        , array[\"this\", \"and\", \"that\"] as `varchar_arr`\n"
+      + "        , array[true, false, false] as `boolean_arr`\n"
+      + "       FROM `example`.`users`\n"
+      + "       LIMIT 1";
 
   @Test
   public void driverIsRegistered() throws SQLException {
@@ -162,6 +179,93 @@ public class LookerDriverTest {
       assertNotNull(test.getDouble(8));
     }
   }
+
+  @Test
+  public void listMeasureResultsCanBeParsedIntoResultSet() throws SQLException {
+    // Set up the driver with some pre-recorded responses from Looker. These are large JSON strings
+    // so have been placed in the LookerTestCommon class to make this test easier to read.
+    Driver driver = new StubbedLookerDriver().withStubbedResponse(
+        LookerTestCommon.stubbedListMeasuresSignature,
+        LookerTestCommon.stubbedListMeasureResponse);
+    Connection connection = driver.connect(LookerTestCommon.getUrl(),
+        LookerTestCommon.getBaseProps());
+
+    ResultSet test = connection.createStatement().executeQuery(TEST_LIST_MEASURE_SQL);
+    ResultSetMetaData rsMetaData = test.getMetaData();
+
+    // verify column types
+    Assert.assertThat(rsMetaData.getColumnCount(), is(3));
+
+    // YEAR
+    Assert.assertThat(rsMetaData.getColumnType(1), is(Types.INTEGER));
+    Assert.assertThat(rsMetaData.getColumnName(1), is("users.created_year"));
+
+    // Names
+    Assert.assertThat(rsMetaData.getColumnType(2), is(Types.ARRAY));
+    Assert.assertThat(rsMetaData.getColumnName(2), is("names"));
+
+    // Ages
+    Assert.assertThat(rsMetaData.getColumnType(3), is(Types.ARRAY));
+    Assert.assertThat(rsMetaData.getColumnName(3), is("ages"));
+
+    // verify every row can be fetched with the appropriate getter method
+    while (test.next()) {
+      assertNotNull(test.getInt(1));
+      assertNotNull(test.getArray(2));
+      assertNotNull(test.getArray(3));
+    }
+  }
+
+  @Test
+  public void canProcessAllArrayTypes() throws SQLException {
+    // Set up the driver with some pre-recorded responses from Looker. These are large JSON strings
+    // so have been placed in the LookerTestCommon class to make this test easier to read.
+    Driver driver = new StubbedLookerDriver().withStubbedResponse(
+        LookerTestCommon.stubbedArrayPrimitivesSig,
+        LookerTestCommon.stubbedArrayPrimitivesResponse);
+    Connection connection = driver.connect(LookerTestCommon.getUrl(),
+        LookerTestCommon.getBaseProps());
+
+    ResultSet test = connection.createStatement().executeQuery(TEST_ARRAY_PRIMITIVES_SQL);
+    ResultSetMetaData rsMetaData = test.getMetaData();
+    int columnCount = rsMetaData.getColumnCount();
+
+    // Verify we got our arrays, skipping index 1 becuase it isn't an array
+    for (int i = 2; i <= columnCount; i++) {
+      Assert.assertThat(rsMetaData.getColumnType(i), is(Types.ARRAY));
+    }
+
+    while (test.next()) {
+      Object actualArrayValue = test.getArray(2).getArray();
+
+      int[] expectedInts = new int[]{1, 2, 3};
+      Assert.assertThat(actualArrayValue, is(equalTo(expectedInts)));
+
+      actualArrayValue = test.getArray(3).getArray();
+      BigDecimal[] expectedDecimals = new BigDecimal[]{
+          BigDecimal.valueOf(1.1), BigDecimal.valueOf(2.2), BigDecimal.valueOf(3.3)
+      };
+      Assert.assertThat(
+          actualArrayValue,
+          is(equalTo(expectedDecimals))
+      );
+
+      String[] expectedStrs = new String[]{"this", "and", null, "that"};
+      actualArrayValue = test.getArray(4).getArray();
+      Assert.assertThat(
+          actualArrayValue,
+          is(equalTo(expectedStrs))
+      );
+
+      boolean[] expectedBools = new boolean[]{true, false, false};
+      actualArrayValue = test.getArray(5).getArray();
+      Assert.assertThat(
+          actualArrayValue,
+          is(equalTo(expectedBools))
+      );
+    }
+  }
+
 
   @Test
   public void canUsePreparedStatement() throws SQLException {

core/src/test/java/org/apache/calcite/avatica/remote/looker/LookerResponseParserTest.java
@@ -17,6 +17,7 @@
 package org.apache.calcite.avatica.remote.looker;
 
 import org.apache.calcite.avatica.ColumnMetaData;
+import org.apache.calcite.avatica.ColumnMetaData.ArrayType;
 import org.apache.calcite.avatica.ColumnMetaData.AvaticaType;
 import org.apache.calcite.avatica.ColumnMetaData.Rep;
 import org.apache.calcite.avatica.util.ByteString;
@@ -37,6 +38,7 @@ import java.sql.Array;
 import java.sql.Date;
 import java.sql.Time;
 import java.sql.Timestamp;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -82,7 +84,6 @@ public class LookerResponseParserTest {
     buildingMap.put(Rep.OBJECT, 1000);
     supportedRepValues = new HashMap(buildingMap);
     buildingMap.clear();
-
     // Unsupported datetime types
     buildingMap.put(Rep.JAVA_SQL_TIME, new Time(1000000));
     buildingMap.put(Rep.JAVA_SQL_DATE, new Date(1000000));
@@ -126,6 +127,47 @@ public class LookerResponseParserTest {
     return ColumnMetaData.dummy(type, false);
   }
 
+  private ColumnMetaData makeArrayMetadata(ColumnMetaData.Rep scalarType) throws IOException {
+    int sqlType;
+    switch (scalarType) {
+    case PRIMITIVE_INT:
+    case INTEGER:
+      sqlType = Types.INTEGER;
+      break;
+    case BYTE:
+    case PRIMITIVE_BYTE:
+      sqlType = Types.SMALLINT;
+      break;
+    case STRING:
+      sqlType = Types.VARCHAR;
+      break;
+    case PRIMITIVE_DOUBLE:
+    case DOUBLE:
+    case NUMBER:
+      sqlType = Types.DOUBLE;
+      break;
+    case FLOAT:
+    case PRIMITIVE_FLOAT:
+      sqlType = Types.FLOAT;
+      break;
+    case BOOLEAN:
+    case PRIMITIVE_BOOLEAN:
+      sqlType = Types.BOOLEAN;
+      break;
+    case PRIMITIVE_LONG:
+    case LONG:
+      sqlType = Types.BIGINT;
+      break;
+    default:
+      throw new IOException("Not implemented yet " + scalarType);
+    }
+
+    ArrayType thing = ColumnMetaData.array(
+        ColumnMetaData.scalar(sqlType, scalarType.toString(), scalarType),
+        "ARRAY", scalarType);
+    return ColumnMetaData.dummy(thing, false);
+  }
+
   @Test
   public void deserializeValueTestingIsExhaustive() {
     HashMap allMap = new HashMap();
@@ -134,6 +176,246 @@ public class LookerResponseParserTest {
 
     Arrays.stream(Rep.values()).forEach(val -> assertNotNull(allMap.get(val)));
   }
+
+  @Test
+  public void deserializeArrayOfString() {
+    String[] testValues = { "This", "is", "a", "test" };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.STRING);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(testValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize ints");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void deserializeArrayOfIntegers() {
+    Integer[] testValues = { 1, 2, 3 };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.INTEGER);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(testValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize ints");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void deserializeArrayOfIntegerWithNulls() {
+    Integer[] testValues = { 1, null, 2, 3 };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.INTEGER);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(testValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize ints");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void deserializeArrayOfPrimitiveInts() {
+    int[] testValues = { 1, 2, 3 };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.PRIMITIVE_INT);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(testValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize ints");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test(expected = IOException.class)
+  public void deserializeArrayOfPrimitiveIntsWithNullsThrowsError() throws IOException {
+    // Yes using an array of Integer here instead of "int" to emulate
+    // scenario where null came in off the wire but metadatatype was primitive int
+    Integer[] testValues = { 1, 2, null, 3 };
+    JsonParser parser = makeTestParserFromValue(testValues);
+    ColumnMetaData meta = makeArrayMetadata(Rep.PRIMITIVE_INT);
+    LookerResponseParser.deserializeArray(parser, meta);
+  }
+
+  @Test
+  public void deserializeArrayOfStringInts() {
+    String[] testValues = { "1", "2", "3" };
+    int[] expectedOutputValues = { 1, 2, 3 };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.INTEGER);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(expectedOutputValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize ints");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void deserializeArrayOfIntsWithNulls() {
+    String[] testValues = { "1", null, "2", null, "3" };
+    Integer[] expectedOutputValues = { 1, null, 2, null, 3 };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.INTEGER);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(expectedOutputValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize integers");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void deserializeArrayOfBooleanPrimitives() {
+    boolean[] testValues = { true, false, false, true };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.PRIMITIVE_BOOLEAN);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(testValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize boolean primitives");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void deserializeArrayOfBooleanStrings() {
+    Boolean[] testValues = { Boolean.TRUE, Boolean.TRUE, null, Boolean.FALSE };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.BOOLEAN);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(testValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize boolean primitives");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test (expected = AssertionError.class)
+  public void deserializeArrayOfByte() {
+    byte[] testValues = { 2, 4, 6 };
+    try {
+      //TODO: b/324088915 - We should look more closely with respect to how we encode on looker side
+      // and for now safer to throw the error.
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.PRIMITIVE_BYTE);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(testValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize boolean primitives");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void deserializeArrayOfLongPrimitives() {
+    long[] testValues = { 123L, 345L };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.PRIMITIVE_LONG);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(testValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize boolean primitives");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void deserializeArrayOfLongs() {
+    Long[] testValues = { null, 1L, 1L, 2L, 5L, 14L, 42L, 132L };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.LONG);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(testValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize boolean primitives");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void deserializeArrayOfLongAsString() {
+    Object[] testValues = { "123", null, "345"};
+    Long[] expectedValues = { 123L, null, 345L };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.LONG);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(expectedValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize boolean primitives");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void deserializeArrayOfFloatPrimitive() {
+    float[] testValues = { 1.2f, 2.2f, 3.1415f };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.PRIMITIVE_FLOAT);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(testValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize boolean primitives");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void deserializeArrayOfFloat() {
+    Float[] testValues = { 1.2f, 2.2f, 3.1415f };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.FLOAT);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(testValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize boolean primitives");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void deserializeArrayOfDoublePrimitive() {
+    double[] testValues = { 1.2, 2.2, 3.1415 };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.PRIMITIVE_DOUBLE);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(testValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize boolean primitives");
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Test
+  public void deserializeArrayOfDouble() {
+    Double[] testValues = { 1.2, 2.2, 3.1415 };
+    try {
+      JsonParser parser = makeTestParserFromValue(testValues);
+      ColumnMetaData meta = makeArrayMetadata(Rep.PRIMITIVE_DOUBLE);
+      Object deserializedVal = LookerResponseParser.deserializeArray(parser, meta);
+      assertThat(deserializedVal, is(equalTo(testValues)));
+    } catch (IOException e) {
+      System.out.println("Unable to deserialize boolean primitives");
+      throw new RuntimeException(e);
+    }
+  }
+
 
   @Test
   public void deserializeValueThrowsErrorOnUnsupportedType() {

commit f3c95a5a39ffdac5ce84594edcad677ab1751db5

[b/268509716] [b/250969501] Add Array support
Fixes checkstyle error that was introduced with commit d1e5168

core/src/main/java/org/apache/calcite/avatica/remote/looker/LookerResponseParser.java
@@ -138,7 +138,7 @@ public class LookerResponseParser {
     ArrayList result = new ArrayList();
     while (parser.nextToken() != JsonToken.END_ARRAY) {
       Object deserialized = deserializeValue(parser, metaData);
-      if (isPrimitive && (deserialized == null)) {
+      if (isPrimitive && deserialized == null) {
         throw new IOException("Primitive array cannot contain null values");
       }
       result.add(deserialized);