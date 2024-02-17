wnob/calcite

Fix for sorting by literals

core/src/main/java/org/apache/calcite/rel/rel2sql/SqlImplementor.java
@@ -612,17 +612,9 @@ public abstract class SqlImplementor {
      */
     public SqlNode orderField(int ordinal) {
       final SqlNode node = field(ordinal);
-      if (node instanceof SqlNumericLiteral
-          && dialect.getConformance().isSortByOrdinal()) {
-        // An integer literal will be wrongly interpreted as a field ordinal.
-        // Convert it to a character literal, which will have the same effect.
-        final String strValue = ((SqlNumericLiteral) node).toValue();
-        return SqlLiteral.createCharString(strValue, node.getParserPosition());
-      }
-      if (node instanceof SqlCall
-          && dialect.getConformance().isSortByOrdinal()) {
-        // If the field is expression and sort by ordinal is set in dialect,
-        // convert it to ordinal.
+      if (!(node instanceof SqlIdentifier) && dialect.getConformance().isSortByOrdinal()) {
+        // If the dialect allows sorting by ordinal, and the field is anything but a named field
+        // reference, prefer to sort by ordinal instead.
         return SqlLiteral.createExactNumeric(
             Integer.toString(ordinal + 1), SqlParserPos.ZERO);
       }

looker-open-source/calcite-avatica

core/src/main/java/org/apache/calcite/rel/rel2sql/SqlImplementor.java
@@ -620,17 +620,9 @@ public abstract class SqlImplementor {
      */
     public SqlNode orderField(int ordinal) {
       final SqlNode node = field(ordinal);
-      if (node instanceof SqlNumericLiteral
-          && dialect.getConformance().isSortByOrdinal()) {
-        // An integer literal will be wrongly interpreted as a field ordinal.
-        // Convert it to a character literal, which will have the same effect.
-        final String strValue = ((SqlNumericLiteral) node).toValue();
-        return SqlLiteral.createCharString(strValue, node.getParserPosition());
-      }
-      if (node instanceof SqlCall
-          && dialect.getConformance().isSortByOrdinal()) {
-        // If the field is expression and sort by ordinal is set in dialect,
-        // convert it to ordinal.
+      if (!(node instanceof SqlIdentifier) && dialect.getConformance().shouldSortByOrdinal()) {
+        // If the dialect allows sorting by ordinal, and the field is anything but a named field
+        // reference, prefer to sort by ordinal instead.
         return SqlLiteral.createExactNumeric(
             Integer.toString(ordinal + 1), SqlParserPos.ZERO);
       }