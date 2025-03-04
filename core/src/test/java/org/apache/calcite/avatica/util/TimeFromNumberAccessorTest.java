8e03622b4fd46be97565ade8f31bc881ec2ceecd
Aryex <alex.le@improving.com>
2025-03-04 11:31:31 -0800

    Rectified differences from 1.24.1 release tag

--- core/src/test/java/org/apache/calcite/avatica/util/TimeFromNumberAccessorTest.java
+++ core/src/test/java/org/apache/calcite/avatica/util/TimeFromNumberAccessorTest.java
@@ -156,26 +156,4 @@ public class TimeFromNumberAccessorTest {
       return value == null;
     }
   }
-
-  /**
-   * Test case for <a href="https://issues.apache.org/jira/browse/CALCITE-6282">[CALCITE-6282]
-   * Avatica ignores time precision when returning TIME results</a>. */
-  @Test public void testPrecision() throws SQLException {
-    final AbstractCursor.Getter getter = new AbstractCursor.Getter() {
-      @Override
-      public Object getObject() throws SQLException {
-        // This is the representation of TIME '12:42:25.34'
-        return 45745340;
-      }
-
-      @Override
-      public boolean wasNull() throws SQLException {
-        return false;
-      }
-    };
-    AbstractCursor.TimeFromNumberAccessor accessor = new AbstractCursor.TimeFromNumberAccessor(
-        getter, null, 2);
-    String string = accessor.getString();
-    assertThat(string, is("12:42:25.34"));
-  }
 }
