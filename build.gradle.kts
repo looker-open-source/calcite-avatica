7063052acf97b18d6d9278c0190bc37a476a7bfe
Aryex <alex.le@improving.com>
2025-03-12 11:47:03 -0700

    fix: Renamed kotlin sdk jar to include the correct commit id

--- build.gradle.kts
+++ build.gradle.kts
@@ -200,7 +200,7 @@ allprojects {
         dependencies {
             "implementation"(platform(project(":bom")))
             // Add the locally bundled LookerSDK fat jar
-            "implementation"(files("../libs/looker-kotlin-sdk-a48011f.jar"))
+            "implementation"(files("../libs/looker-kotlin-sdk-100e3c9.jar"))
         }
     }
     if (!skipAutostyle) {
